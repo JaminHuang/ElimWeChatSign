cf518ee6717baec1c2d088c39c92a302d527387d
JaminHuang <822541147@qq.com>
2016-11-10 17:04:32 +0800

    添加

---
+++ SourceCode/ElimWeChatSign.Model/Res/ResUserSign.cs
@@ -0,0 +1,33 @@
+﻿namespace ElimWeChatSign.Model
+{
+	/// <summary>
+	/// 用户签到输出类
+	/// </summary>
+	public class ResUserSign
+	{
+		/// <summary>
+		/// 签到标识
+		/// </summary>
+		public string SignId { get; set; }
+		/// <summary>
+		/// 用户标识
+		/// </summary>
+		public string UserId { get; set; }
+		/// <summary>
+		/// 计划标识
+		/// </summary>
+		public string PlanId { get; set; }
+		/// <summary>
+		/// 签到类型[1:读经;2:读书]
+		/// </summary>
+		public int SignType { get; set; }
+		/// <summary>
+		/// 签到时间
+		/// </summary>
+		public string SignDate { get; set; }
+		/// <summary>
+		/// 最后修改时间
+		/// </summary>
+		public string UpdateTime { get; set; }
+	}
+}
